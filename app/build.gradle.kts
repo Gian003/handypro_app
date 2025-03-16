90b3d9bebc1bba1927cbba518aa6b2bc4b14302f
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-16 22:10:35 +0800

    changed example to ucucite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.handypro_app"
+    namespace = "com.ucucite.handypro_app"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.handypro_app"
+        applicationId = "com.ucucite.handypro_app"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -33,7 +33,7 @@ android {
 
 dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
