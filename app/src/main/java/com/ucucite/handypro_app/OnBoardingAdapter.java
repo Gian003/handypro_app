ea52cefb8f906817b06a50dacf57f1167f3f0d2b
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-19 04:08:37 +0800

    added OnBoardingAdapter

---
+++ app/src/main/java/com/ucucite/handypro_app/OnBoardingAdapter.java
@@ -0,0 +1,43 @@
+package com.ucucite.handypro_app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+    public class OnBoardingAdapter extends RecyclerView.Adapter<OnBoardingAdapter.ViewHolder> {
+    private int[] layouts;
+
+    public OnBoardingAdapter(int[] layouts) {
+        this.layouts = layouts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(layouts[viewType], parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // No binding needed as layouts are static
+    }
+
+    @Override
+    public int getItemCount() {
+        return layouts.length;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
