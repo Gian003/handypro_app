cd710be0e10f2949d67cad7b9df32a0af3fca12c
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-20 02:36:33 +0800

    configured to display layout

--- app/src/main/java/com/ucucite/handypro_app/Main_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/Main_Activity.java
@@ -7,18 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Main_Activity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+    private OnBoardingAdapter onBoardingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.OnBoarding1), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        int[] layouts = new int[]{
+                R.layout.activity_onboarding1,
+                R.layout.activity_onboarding2
+        };
+
+        OnBoardingAdapter adapter = new OnBoardingAdapter(layouts);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("First");
+                    break;
+                case 1:
+                    tab.setText("Second");
+                    break;
+            }
+        }).attach();
     }
 }
