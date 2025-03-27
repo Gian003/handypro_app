a649a0a6886c35d7e747ee48a56a9bb13a786a57
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-27 23:34:34 +0800

    connects the view for login_activity layout and java

--- app/src/main/java/com/ucucite/handypro_app/Login_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/Login_Activity.java
@@ -1,6 +1,12 @@
 package com.ucucite.handypro_app;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
 }
