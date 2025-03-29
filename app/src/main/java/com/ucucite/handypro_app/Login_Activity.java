d7e77d1b12149cff531f6d501d3d8385290dc67d
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-29 21:23:16 +0800

    added configurations

--- app/src/main/java/com/ucucite/handypro_app/Login_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/Login_Activity.java
@@ -1,12 +1,21 @@
 package com.ucucite.handypro_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_log_in);
+
+        TextView SignUp_btn = findViewById(R.id.SignUp_Btn);
+
+        SignUp_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(Login_Activity.this, SignUp_Activity.class);
+            startActivity(intent);
+        });
     }
 }
