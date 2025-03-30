c96a22e4583c478b43983d41efda77466d720ef7
Gian003 <dev.Rodriguez2111@gmail.com>
2025-03-31 01:57:21 +0800

    added SignUp activity

--- app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
@@ -1,5 +1,6 @@
 package com.ucucite.handypro_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class SignUp_Activity extends AppCompatActivity {
 
     @Override
@@ -17,5 +20,11 @@ public class SignUp_Activity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
+        TextView LogIn_btn = findViewById(R.id.LogIn_Btn);
+
+        LogIn_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUp_Activity.this, Login_Activity.class);
+            startActivity(intent);
+        });
     }
 }
