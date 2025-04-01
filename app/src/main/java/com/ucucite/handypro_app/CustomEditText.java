1cd37cd5509c5ce39979b776387b1f7eb14ef86c
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-02 01:44:24 +0800

    added CustomEditText to handle manual pass logic

---
+++ app/src/main/java/com/ucucite/handypro_app/CustomEditText.java
@@ -0,0 +1,31 @@
+package com.ucucite.handypro_app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class CustomEditText extends TextInputEditText {
+
+    public CustomEditText(Context context) {
+        super(context);
+    }
+
+    public CustomEditText(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public CustomEditText(Context context, AttributeSet attributeSet, int defStyleAttribute) {
+        super(context, attributeSet, defStyleAttribute);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean performClick () {
+        return super.performClick();
+    }
+}
