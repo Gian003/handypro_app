99ce10ebe13683f23c50eb3182854785ca56f254
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-02 19:04:15 +0800

    removed pass handler to class to handle more and maintain easily

--- app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
@@ -13,8 +13,7 @@ import java.util.Objects;
 
 public class SignUp_Activity extends AppCompatActivity {
 
-    private boolean isPasswordVisible = false;
-    private boolean isConfirmPasswordVisible = false;
+    PassVisibilityHandler passVisibilityHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,48 +30,8 @@ public class SignUp_Activity extends AppCompatActivity {
         CustomEditText InitialPassword = findViewById(R.id.Password_Initial_Input);
         CustomEditText ConfirmPassword = findViewById(R.id.Password_Confirm_Input);
 
-        setUpPasswordVisibilityToggle(InitialPassword, true);
-        setUpPasswordVisibilityToggle(ConfirmPassword, false);
+        passVisibilityHandler.setUpPasswordVisibilityToggle(InitialPassword, true);
+        passVisibilityHandler.setUpPasswordVisibilityToggle(ConfirmPassword, false);
     }
 
-    private void setUpPasswordVisibilityToggle(CustomEditText passwordField, boolean isInitialPassword) {
-        passwordField.setOnTouchListener((v, event) -> {
-            final int DRAWABLE_RIGHT = 2;
-
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-               if (passwordField.getCompoundDrawables()[DRAWABLE_RIGHT] != null) {
-                   if (event.getRawX() >= (passwordField.getRight() - passwordField.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-
-                       boolean isVisible = isInitialPassword ? isPasswordVisible : isConfirmPasswordVisible;
-
-                       if (isVisible) {
-                           passwordField.setTransformationMethod(new android.text.method.PasswordTransformationMethod());
-                           passwordField.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.pass_visibility_off, 0);
-                       } else {
-                           passwordField.setTransformationMethod(null);
-                           passwordField.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.pass_visibility_on, 0);
-                       }
-
-                       if (passwordField.getText() != null) {
-                           passwordField.setSelection(passwordField.getText().length());
-                       }
-
-                       if (isInitialPassword) {
-                           isPasswordVisible = !isPasswordVisible;
-                       } else {
-                           isConfirmPasswordVisible = !isConfirmPasswordVisible;
-                       }
-
-                       v.performClick();
-                       return true;
-                   }
-               }
-            }
-            return false;
-        });
-
-        passwordField.setOnClickListener(v -> {
-
-        });
-    }
 }
