4f5763f66a7d578a765185c33697368d475468ed
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-04 00:48:37 +0800

    added Input handler where if the user presses "OK" it will move to the next Input and if on the last one it will then clears the keyboard or hides it.

--- app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/SignUp_Activity.java
@@ -1,7 +1,12 @@
 package com.ucucite.handypro_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +34,66 @@ public class SignUp_Activity extends AppCompatActivity {
         //password visibility toggle from PassVisibilityHandler class
         PassVisibilityHandler.setUpPasswordVisibilityToggle(InitialPassword, true);
         PassVisibilityHandler.setUpPasswordVisibilityToggle(ConfirmPassword, false);
-    }
 
+        //input field if pressed "OK" or "Enter" moves to another input field
+        EditText NextInput_Field1 = findViewById(R.id.SignUp_FNameInput);
+        EditText NextInput_Field2 = findViewById(R.id.SignUp_LNameInput);
+        EditText NextInput_Field3 = findViewById(R.id.SignUp_EmailInput);
+        EditText NextInput_Field4 = findViewById(R.id.SignUp_NumberInput);
+        //Initial and Confirm Password are not written here again to avoid redundancy
+
+
+        //input field if pressed "OK" or "Enter" moves to another input field
+        NextInput_Field1.setOnEditorActionListener((v, actionId, event) -> {
+           if (actionId == EditorInfo.IME_ACTION_NEXT) {
+               NextInput_Field2.requestFocus();
+               return true;
+           }
+           return false;
+        });
+
+        NextInput_Field2.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                NextInput_Field3.requestFocus();
+                return true;
+            }
+            return false;
+        });
+
+        NextInput_Field3.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                NextInput_Field4.requestFocus();
+                return true;
+
+            }
+            return false;
+        });
+
+        NextInput_Field4.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                ConfirmPassword.requestFocus();
+                return true;
+            }
+            return false;
+        });
+
+        ConfirmPassword.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                InitialPassword.requestFocus();
+                return true;
+            }
+            return false;
+        });
+
+        //Hides the keyboard when pressed "OK" or "Enter" to confirm password
+        ConfirmPassword.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                ConfirmPassword.clearFocus();;
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(ConfirmPassword.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+    }
 }
