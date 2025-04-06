85c4af13a539d74a7366d9fbe57ba05739adf863
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-06 14:00:20 +0800

    added custom search view for better maintenance

--- app/src/main/java/com/ucucite/handypro_app/Home_Activity.java
+++ app/src/main/java/com/ucucite/handypro_app/Home_Activity.java
@@ -1,9 +1,14 @@
 package com.ucucite.handypro_app;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +20,5 @@ public class Home_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
