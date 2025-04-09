254a7f0c0560f6e56ca32ceab33f0fe92e584661
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-10 01:50:36 +0800

    added reccomend section activity on home

---
+++ app/src/main/java/com/ucucite/handypro_app/HomeReccItem.java
@@ -0,0 +1,49 @@
+package com.ucucite.handypro_app;
+
+public class HomeReccItem {
+    private int image;
+    private String Service;
+    private String worker;
+    private double rating;
+    private int reviews;
+    private double Price;
+    private boolean hasDiscount;
+
+    public HomeReccItem (int image, String Service, String worker, double rating, int reviews, double Price, boolean hasDiscount) {
+        this.image = image;
+        this.Service = Service;
+        this.worker = worker;
+        this.rating = rating;
+        this.reviews = reviews;
+        this.Price = Price;
+        this.hasDiscount = hasDiscount;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getService() {
+        return Service;
+    }
+
+    public String getWorker() {
+        return worker;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getReviews() {
+        return reviews;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public boolean hasDiscount() {
+        return hasDiscount;
+    }
+}
