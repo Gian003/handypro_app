aa04cb3190c93466b0c169347533e2d6542d9d40
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-14 02:51:26 +0800

    added ui for bottom nav and logic

---
+++ app/src/main/java/com/ucucite/handypro_app/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ucucite.handypro_app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void loadFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment(), false);
+        }
+
+        BottomNavigationView bottomNav= findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int id = item.getItemId();
+                if (id == R.id.nav_home_menu) {
+                    selectedFragment = new HomeFragment();
+                }
+                loadFragment(selectedFragment, true);
+                return true;
+            }
+        });
+    }
+}
