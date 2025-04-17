b42a41241c2b9618155ac5b216e0cf061af874c7
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-18 02:39:29 +0800

    added onclicklistener for filtering on category

--- app/src/main/java/com/ucucite/handypro_app/CategoryServicesAdapter.java
+++ app/src/main/java/com/ucucite/handypro_app/CategoryServicesAdapter.java
@@ -5,15 +5,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.ucucite.handypro_app.CategoryServicesItem;
 
 import java.util.List;
 
-public class CategoryServicesAdapter extends RecyclerView.Adapter<CategoryServicesAdapter.ViewHolder>{
-    private List<CategoryServicesItem> categoryServicesItems;
+public class CategoryServicesAdapter extends RecyclerView.Adapter<CategoryServicesAdapter.ViewHolder> {
 
-    public CategoryServicesAdapter(List<CategoryServicesItem> categoryServicesItems) {
+    private List<CategoryServicesItem> categoryServicesItems;
+    private OnCategoryClickListener listener;
+
+    public CategoryServicesAdapter(List<CategoryServicesItem> categoryServicesItems, OnCategoryClickListener listener) {
         this.categoryServicesItems = categoryServicesItems;
+        this.listener = listener;
+    }
+
+    public interface OnCategoryClickListener {
+        void onCategoryClick(String categoryLabel);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -24,17 +34,23 @@ public class CategoryServicesAdapter extends RecyclerView.Adapter<CategoryServic
         }
     }
 
+    @NonNull
     @Override
-    public CategoryServicesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.home_services_item, parent, false);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_services_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(CategoryServicesAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         CategoryServicesItem item = categoryServicesItems.get(position);
         holder.icon.setImageResource(item.getImage());
+
+        holder.icon.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onCategoryClick(item.getLabel());
+            }
+        });
     }
 
     @Override
