10baac71e136b4d3aa4c7d23905a3fe6d2a37699
Gian003 <dev.Rodriguez2111@gmail.com>
2025-04-18 02:40:37 +0800

    added label for filtering

--- app/src/main/java/com/ucucite/handypro_app/CategoryServicesItem.java
+++ app/src/main/java/com/ucucite/handypro_app/CategoryServicesItem.java
@@ -2,12 +2,18 @@ package com.ucucite.handypro_app;
 
 public class CategoryServicesItem {
     private int image;
+    private String label;
 
-    public CategoryServicesItem(int image) {
+    public CategoryServicesItem(int image, String label) {
         this.image = image;
+        this.label = label;
     }
 
     public int getImage() {
         return image;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
